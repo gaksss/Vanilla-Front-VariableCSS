df776d5cfc2a157e1f23b51887431df7d478194f
gaksss <clement.carlier2@gmail.com>
2024-11-22 14:24:02 +0100

    Exo2 fini

---
+++ EXERCICE2/script.js
@@ -0,0 +1,13 @@
+// Définir la variable permettant de récupérer l'input de type color
+
+const pick = document.querySelector("input");
+const r = document.querySelector(":root");
+pick.addEventListener("change", handleChangeColor);
+
+function handleChangeColor(event) {
+  r.style.setProperty("--main-color", pick.value);
+}
+/* A cette variable, appliquer la méthode (addEventListener) écoutant l'événement de type change et jouant la fonction permettant d'ajouter une propriété de style (SetProperty) au document :
+- le nom de la propriété est la variable CSS --main-color 
+- La valeur est définie par la valeur de l'input
+*/
