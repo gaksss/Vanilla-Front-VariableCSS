9baa10f6caf3689007b109a3532c3c1124af9549
gaksss <clement.carlier2@gmail.com>
2024-11-25 09:37:36 +0100

    correction exo3

--- EXERCICE3/script.js
+++ EXERCICE3/script.js
@@ -1,24 +1,21 @@
-
 const inputs = document.querySelectorAll("input");
 
-const root = document.documentElement;
+const root = document.querySelector(":root");
 
+inputs.forEach((input) => {
+  input.addEventListener("input", handlePropertyChange);
+});
 
 function handlePropertyChange(event) {
-  
-  const name = event.target.name; // spacing, blur, color
-  const value = event.target.value;
-  const suffix = event.target.dataset.sizing || ""; 
-
-  // Détermine la propriété CSS à mettre à jour
-  if (name === "color") {
-    root.style.setProperty(`--color`, value);
+  // console.log(event.target.name);
+  const unite = event.target.dataset.sizing;
+  if (event.target.name === "color") {
+    root.style.setProperty("--color", event.target.value);
+  } else if (event.target.name === "spacing") {
+    root.style.setProperty("--spacing", event.target.value + unite);
   } else {
-    root.style.setProperty("--"+ name, value + suffix);
+    root.style.setProperty("--blur", event.target.value + unite);
   }
 }
 
-// Ajoute l'événement à chaque input
-inputs.forEach((input) => {
-  input.addEventListener("change", handlePropertyChange);
-});
+
